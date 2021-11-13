354d60f3831bac160bad61f46ffe777cff45ee4c
rshreeebastav <94220498+rshreeebastav@users.noreply.github.com>
2021-11-13 14:54:22 +0530

    Add files via upload

---
+++ Demo.py
@@ -0,0 +1,47 @@
+# PBL : Auto File Organization
+# Name : Rahul Shreeebastav
+# Roll no:10
+# Class: 3CEC
+# Enrollment_No: 20SOECE11094
+
+
+import os
+import shutil
+
+# Reading the folders file
+folderpath=r"D:\Study material\Semester 3\Python-I\Python Project\All files"
+os.chdir(folderpath)
+os.getcwd()
+
+
+# CHeck the files
+os.listdir()
+
+
+# get extension
+list_extension=[]
+for fl in os.listdir():
+    extension=fl.split(".")[-1]
+    list_extension.append(extension)
+
+list_extension=set(list_extension)
+print(list_extension)
+print(len(list_extension))
+
+# Create a folder on the desktop
+path = os.environ["Userprofile"]+"\\"+"Desktop"+"\\"+"Rahul"
+print(path)
+os.mkdir(path)
+try:
+    shutil.rmtree(path)
+    os.mkdir(path)
+except:
+    os.mkdir(path)
+
+# We can transfer the file with specified folder
+for ex in list_extension:
+    print(ex,end=",")
+    os.mkdir(path+"\\"+ex)
+    for fl in os.listdir():
+        if ex in fl:
+            shutil.copy(fl,path+"\\"+ex)
